09ce033312713ca17ab705c209f5c79c0a038dc3
Simon Sickle <simon@simonsickle.com>
2015-07-07 23:53:35 -0700

    flounder: fix 'internal problem with your device' message
    thanks to @maniac103 for the tip on where to look for proper fix
    
    Change-Id: I5c73da38699b2d5ccf916d172ed65d5af334784c
    Signed-off-by: Simon Sickle <simon@simonsickle.com>
    
    Conflicts:
    	cm.mk

--- eos.mk
+++ eos.mk
@@ -40,6 +40,11 @@ TARGET_KERNEL_CONFIG := flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY47X/1849464:user/release-keys \
+    PRIVATE_BUILD_DESC="flounder-user 5.1.1 LMY47X 1849464 release-keys"
+
 # Inherit some common Eos stuff.
 $(call inherit-product, vendor/eos/config/common_full_tablet_wifionly.mk)
 $(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
